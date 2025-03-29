4b9adc730768b143a0911b3dfff0ab66a37f4478
archos <archos@archoslinux.cz>
2025-03-29 16:06:39 +0100

    Přidána klávesová zkratka pro otevření terminálu

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.textwidth = 80
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.textwidth = 90 -- Tvrdé zalamování po 90 znacích
+    vim.opt_local.formatoptions = "t" -- Automatické zalamování při psaní
+  end,
+})
