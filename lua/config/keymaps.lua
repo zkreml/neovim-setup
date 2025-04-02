7bf9a9bcc81f45d2b5bd52137c8f33ead62a781b
archos <archos@archoslinux.cz>
2025-04-02 19:55:08 +0200

    Přidány klávesové zkratky

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>tt", ":terminal<CR>", { desc = "Otevři terminál" })
+vim.keymap.set("n", "<leader>tt", ":vsplit | terminal<CR>", { desc = "Otevři terminál vpravo" })
